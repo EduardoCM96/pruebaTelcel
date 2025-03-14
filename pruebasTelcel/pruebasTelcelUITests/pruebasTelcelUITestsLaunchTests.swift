0335da5bba00c3ba54379c5dcc32e1e599e60d9c
Eduardo Carranza <88063305+EduardoCM96@users.noreply.github.com>
2025-03-14 14:58:58 -0600

    commit init

---
+++ pruebasTelcel/pruebasTelcelUITests/pruebasTelcelUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pruebasTelcelUITestsLaunchTests.swift
+//  pruebasTelcelUITests
+//
+//  Created by Eduardo Carranza maqueda on 14/03/25.
+//
+
+import XCTest
+
+final class pruebasTelcelUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
